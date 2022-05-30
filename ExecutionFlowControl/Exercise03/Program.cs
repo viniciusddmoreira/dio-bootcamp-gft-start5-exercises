52faf23005b2ae5d131364e854a6bce71058e3f2
viniciusddmoreira <viniciusdanielmoreira@gmail.com>
2022-05-30 19:23:01 -0300

    Add exercise03 project files

---
+++ ExecutionFlowControl/Exercise03/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Exercise03
+{
+    public class Program
+    {
+        static void Demo1()
+        {
+            Boolean condicao = true;
+            int valor;
+
+            do
+            {
+                Console.WriteLine("Digite um valor, 0 para sair");
+                valor = int.Parse(Console.ReadLine());
+
+                if (valor == 0)
+                {
+                    Console.WriteLine("Você saiu da aplicação");
+                    condicao = false;
+                }
+                else
+                {
+                    Console.WriteLine("O valor informado é: " + valor);
+
+                }
+            } while (condicao == true);
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[] lista = { 1, 2, 3, 4, 5 };
+
+            foreach (int numero in lista)
+            {
+
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
+
+
+
