8cb0506f0cc82ffba7d261169c07fd07d20bcf6b
viniciusddmoreira <viniciusdanielmoreira@gmail.com>
2022-05-30 19:31:37 -0300

    Add exercise04 project files

---
+++ ExecutionFlowControl/Exercise04/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean condicao = true;
+            int valor;
+
+            do
+            {
+                Console.WriteLine("Digite um valor, 0 para sair");
+                valor = int.Parse(Console.ReadLine());
+
+                if (valor == 0)
+                {
+                    Console.WriteLine("Você saiu da aplicação");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("O valor informado é: " + valor);
+                    continue;
+                }
+            } while (condicao == true);
+        }
+    }
+}
