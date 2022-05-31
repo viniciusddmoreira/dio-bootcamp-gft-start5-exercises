23e58b84601fd01fb85d01c7e00fc877375058e2
viniciusddmoreira <viniciusdanielmoreira@gmail.com>
2022-05-31 14:06:01 -0300

    Add CollectionFundamentalsAndLINQ project files

---
+++ CollectionFundamentalsAndLINQ/Collections/Models/Person.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Collections.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public List<string> Enderecos { get; set; }
+    }
+}
