0099d97a2e4a6e45f9c7d50b0f24d0a902eacc5d
viniciusddmoreira <viniciusdanielmoreira@gmail.com>
2022-05-30 23:53:28 -0300

    Add Average project files

---
+++ InitialChallenges/Average/Program.cs
@@ -0,0 +1,20 @@
+﻿//Abaixo segue um exemplo de código que você pode ou não utilizar
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C;
+            A = double.Parse(Console.ReadLine());
+            B = double.Parse(Console.ReadLine());
+            C = double.Parse(Console.ReadLine());
+            
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+            double media = (A*0.2)+(B*0.3)+(C*0.5);
+            Console.WriteLine($"MEDIA = {media.ToString("0.0")}");
+            Console.ReadKey();
+        }
+    }
+}
