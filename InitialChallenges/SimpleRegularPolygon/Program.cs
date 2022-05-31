5ba33a900b2a971572cf0fefbea12d5046076801
viniciusddmoreira <viniciusdanielmoreira@gmail.com>
2022-05-31 00:42:48 -0300

    Add SimpleRegularPolygon project files

---
+++ InitialChallenges/SimpleRegularPolygon/Program.cs
@@ -0,0 +1,10 @@
+﻿// Abaixo segue um exemplo de código que você pode ou não utilizar
+class SimpleRegularPolygon
+{
+    static void Main(string[] args)
+    {
+        //TODO: Complete os espaços em branco com uma possível solução para o desafio
+        string[] v = Console.ReadLine().Split();
+        Console.WriteLine(int.Parse(v[0]) * int.Parse(v[1]));
+    }
+}
